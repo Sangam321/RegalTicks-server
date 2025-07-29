561ab3b7fef3bc81042b67de5dc7642e5aa25e7e
Sangam Basnet <sangambasnet1800@gmail.com>
2025-07-29 21:17:46 +0545

    otp function update

--- utils/otpGenerator.js
+++ utils/otpGenerator.js
@@ -1,9 +1,9 @@
 export const generateOTP = () => {
-    // Generate a 6-digit numeric OTP
+
     return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
 export const generateOTPExpiry = () => {
-    // Generate expiry date (10 minutes from now)
+
     return new Date(Date.now() + 10 * 60 * 1000);
 };
